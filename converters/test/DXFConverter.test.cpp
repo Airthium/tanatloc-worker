1ce5296b3f39442258844469ddc9b729e35729c5
Simon Garnotel <simon.garnotel@gmail.com>
2020-11-03 15:48:28 +0100

    test

--- converters/test/DXFConverter.test.cpp
+++ converters/test/DXFConverter.test.cpp
@@ -16,4 +16,16 @@ TEST_CASE("DXFConverter") {
 
     dxfConverter.setOutput("output");
   }
+
+  SECTION("convert") {
+    DXFConverter dxfConverter = DXFConverter();
+    dxfConverter.setInput("./test/assets/pipe.dxf");
+    dxfConverter.convert();
+  }
+
+  SECTION("write") {
+    DXFConverter dxfConverter = DXFConverter();
+    dxfConverter.setOutput("pipe.brep");
+    dxfConverter.write();
+  }
 }
