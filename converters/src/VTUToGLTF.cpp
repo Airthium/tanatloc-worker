ecab54736713245895fd6ecd4ca3154a6288f85d
Simon Garnotel <simon.garnotel@gmail.com>
2022-05-19 17:41:10 +0200

    VTU too long

--- converters/src/VTUToGLTF.cpp
+++ converters/src/VTUToGLTF.cpp
@@ -125,96 +125,95 @@ bool writeOne(const RData &result, const std::string &gltfFile,
               const double radius) {
   MainDocument mainDocument;
 
-  // // Tetrahedra
-  // TopoDS_Compound tethradedra;
-  // TopoDS_Builder tethradedraBuilder;
-  // tethradedraBuilder.MakeCompound(tethradedra);
+  // Tetrahedra
+  TopoDS_Compound tethradedra;
+  TopoDS_Builder tethradedraBuilder;
+  tethradedraBuilder.MakeCompound(tethradedra);
 
-  // TDF_Label tethradedraLabel = mainDocument.addShape(tethradedra,
-  // "Tetrahedra");
+  TDF_Label tethradedraLabel = mainDocument.addShape(tethradedra, "Tetrahedra");
 
-  // for (uint i = 0; i < result.tetrahedra.size(); i++) {
-  //   Tetrahedron tetrahedron = result.tetrahedra.at(i);
+  for (uint i = 0; i < result.tetrahedra.size(); i++) {
+    Tetrahedron tetrahedron = result.tetrahedra.at(i);
 
-  //   // Indices
-  //   uint index1 = tetrahedron.getI1();
-  //   uint index2 = tetrahedron.getI2();
-  //   uint index3 = tetrahedron.getI3();
-  //   uint index4 = tetrahedron.getI4();
+    // Indices
+    uint index1 = tetrahedron.getI1();
+    uint index2 = tetrahedron.getI2();
+    uint index3 = tetrahedron.getI3();
+    uint index4 = tetrahedron.getI4();
 
-  //   // Data
-  //   double data1 = result.values.at(index1);
-  //   double data2 = result.values.at(index2);
-  //   double data3 = result.values.at(index3);
-  //   double data4 = result.values.at(index4);
+    // Data
+    double data1 = result.values.at(index1);
+    double data2 = result.values.at(index2);
+    double data3 = result.values.at(index3);
+    double data4 = result.values.at(index4);
 
-  //   // Vertices
-  //   Vertex v1 = result.vertices.at(index1);
-  //   Vertex v2 = result.vertices.at(index2);
-  //   Vertex v3 = result.vertices.at(index3);
-  //   Vertex v4 = result.vertices.at(index4);
+    // Vertices
+    Vertex v1 = result.vertices.at(index1);
+    Vertex v2 = result.vertices.at(index2);
+    Vertex v3 = result.vertices.at(index3);
+    Vertex v4 = result.vertices.at(index4);
 
-  //   // Points
-  //   gp_Pnt point1(v1.X(), v1.Y(), v1.Z());
-  //   gp_Pnt point2(v2.X(), v2.Y(), v2.Z());
-  //   gp_Pnt point3(v3.X(), v3.Y(), v3.Z());
-  //   gp_Pnt point4(v4.X(), v4.Y(), v4.Z());
+    // Points
+    gp_Pnt point1(v1.X(), v1.Y(), v1.Z());
+    gp_Pnt point2(v2.X(), v2.Y(), v2.Z());
+    gp_Pnt point3(v3.X(), v3.Y(), v3.Z());
+    gp_Pnt point4(v4.X(), v4.Y(), v4.Z());
 
-  //   // Vertices
-  //   BRepBuilderAPI_MakeVertex vertex1(point1);
-  //   BRepBuilderAPI_MakeVertex vertex2(point2);
-  //   BRepBuilderAPI_MakeVertex vertex3(point3);
-  //   BRepBuilderAPI_MakeVertex vertex4(point4);
+    // Vertices
+    BRepBuilderAPI_MakeVertex vertex1(point1);
+    BRepBuilderAPI_MakeVertex vertex2(point2);
+    BRepBuilderAPI_MakeVertex vertex3(point3);
+    BRepBuilderAPI_MakeVertex vertex4(point4);
 
-  //   // Edges
-  //   BRepBuilderAPI_MakeEdge edge1(vertex1.Vertex(), vertex2.Vertex());
-  //   BRepBuilderAPI_MakeEdge edge2(vertex1.Vertex(), vertex3.Vertex());
-  //   BRepBuilderAPI_MakeEdge edge3(vertex1.Vertex(), vertex4.Vertex());
-  //   BRepBuilderAPI_MakeEdge edge4(vertex2.Vertex(), vertex3.Vertex());
-  //   BRepBuilderAPI_MakeEdge edge5(vertex2.Vertex(), vertex4.Vertex());
-  //   BRepBuilderAPI_MakeEdge edge6(vertex3.Vertex(), vertex4.Vertex());
+    // Edges
+    BRepBuilderAPI_MakeEdge edge1(vertex1.Vertex(), vertex2.Vertex());
+    BRepBuilderAPI_MakeEdge edge2(vertex1.Vertex(), vertex3.Vertex());
+    BRepBuilderAPI_MakeEdge edge3(vertex1.Vertex(), vertex4.Vertex());
+    BRepBuilderAPI_MakeEdge edge4(vertex2.Vertex(), vertex3.Vertex());
+    BRepBuilderAPI_MakeEdge edge5(vertex2.Vertex(), vertex4.Vertex());
+    BRepBuilderAPI_MakeEdge edge6(vertex3.Vertex(), vertex4.Vertex());
 
-  //   // Cylinder
-  //   TopoDS_Shape cylinder1 = makeCylinder(radius, edge1);
-  //   TopoDS_Shape cylinder2 = makeCylinder(radius, edge2);
-  //   TopoDS_Shape cylinder3 = makeCylinder(radius, edge3);
-  //   TopoDS_Shape cylinder4 = makeCylinder(radius, edge4);
-  //   TopoDS_Shape cylinder5 = makeCylinder(radius, edge5);
-  //   TopoDS_Shape cylinder6 = makeCylinder(radius, edge6);
+    // Cylinder
+    TopoDS_Shape cylinder1 = makeCylinder(radius, edge1);
+    TopoDS_Shape cylinder2 = makeCylinder(radius, edge2);
+    TopoDS_Shape cylinder3 = makeCylinder(radius, edge3);
+    TopoDS_Shape cylinder4 = makeCylinder(radius, edge4);
+    TopoDS_Shape cylinder5 = makeCylinder(radius, edge5);
+    TopoDS_Shape cylinder6 = makeCylinder(radius, edge6);
 
-  //   tethradedraBuilder.Add(tethradedra, cylinder1);
-  //   tethradedraBuilder.Add(tethradedra, cylinder2);
-  //   tethradedraBuilder.Add(tethradedra, cylinder3);
-  //   tethradedraBuilder.Add(tethradedra, cylinder4);
-  //   tethradedraBuilder.Add(tethradedra, cylinder5);
-  //   tethradedraBuilder.Add(tethradedra, cylinder6);
+    tethradedraBuilder.Add(tethradedra, cylinder1);
+    tethradedraBuilder.Add(tethradedra, cylinder2);
+    tethradedraBuilder.Add(tethradedra, cylinder3);
+    tethradedraBuilder.Add(tethradedra, cylinder4);
+    tethradedraBuilder.Add(tethradedra, cylinder5);
+    tethradedraBuilder.Add(tethradedra, cylinder6);
 
-  //   std::string name1 = "Tetrahedron" + std::to_string(i) + ";1;{" +
-  //                       std::to_string(data1) + "," + std::to_string(data2) +
-  //                       "}";
-  //   std::string name2 = "Tetrahedron" + std::to_string(i) + ";2;{" +
-  //                       std::to_string(data1) + "," + std::to_string(data3) +
-  //                       "}";
-  //   std::string name3 = "Tetrahedron" + std::to_string(i) + ";3;{" +
-  //                       std::to_string(data1) + "," + std::to_string(data4) +
-  //                       "}";
-  //   std::string name4 = "Tetrahedron" + std::to_string(i) + ";4;{" +
-  //                       std::to_string(data2) + "," + std::to_string(data3) +
-  //                       "}";
-  //   std::string name5 = "Tetrahedron" + std::to_string(i) + ";5;{" +
-  //                       std::to_string(data2) + "," + std::to_string(data4) +
-  //                       "}";
-  //   std::string name6 = "Tetrahedron" + std::to_string(i) + ";6;{" +
-  //                       std::to_string(data3) + "," + std::to_string(data4) +
-  //                       "}";
+    std::string name1 = "Tetrahedron" + std::to_string(i) + ";1;{" +
+                        std::to_string(data1) + "," + std::to_string(data2) +
+                        "}";
+    std::string name2 = "Tetrahedron" + std::to_string(i) + ";2;{" +
+                        std::to_string(data1) + "," + std::to_string(data3) +
+                        "}";
+    std::string name3 = "Tetrahedron" + std::to_string(i) + ";3;{" +
+                        std::to_string(data1) + "," + std::to_string(data4) +
+                        "}";
+    std::string name4 = "Tetrahedron" + std::to_string(i) + ";4;{" +
+                        std::to_string(data2) + "," + std::to_string(data3) +
+                        "}";
+    std::string name5 = "Tetrahedron" + std::to_string(i) + ";5;{" +
+                        std::to_string(data2) + "," + std::to_string(data4) +
+                        "}";
+    std::string name6 = "Tetrahedron" + std::to_string(i) + ";6;{" +
+                        std::to_string(data3) + "," + std::to_string(data4) +
+                        "}";
 
-  //   mainDocument.addComponent(tethradedraLabel, cylinder1, name1);
-  //   mainDocument.addComponent(tethradedraLabel, cylinder2, name2);
-  //   mainDocument.addComponent(tethradedraLabel, cylinder3, name3);
-  //   mainDocument.addComponent(tethradedraLabel, cylinder4, name4);
-  //   mainDocument.addComponent(tethradedraLabel, cylinder5, name5);
-  //   mainDocument.addComponent(tethradedraLabel, cylinder6, name6);
-  // }
+    mainDocument.addComponent(tethradedraLabel, cylinder1, name1);
+    mainDocument.addComponent(tethradedraLabel, cylinder2, name2);
+    mainDocument.addComponent(tethradedraLabel, cylinder3, name3);
+    mainDocument.addComponent(tethradedraLabel, cylinder4, name4);
+    mainDocument.addComponent(tethradedraLabel, cylinder5, name5);
+    mainDocument.addComponent(tethradedraLabel, cylinder6, name6);
+  }
 
   // Triangles
   TopoDS_Compound triangles;
